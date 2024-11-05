7be481d9fcbd7fc140f76efda60f665677ca071e
Refat Jamil <rifatjamil54@gmail.com>
2024-11-05 23:59:30 +0600

    learn consumer

--- my_project/app/consumers.py
+++ my_project/app/consumers.py
@@ -5,13 +5,18 @@ from channels.exceptions import StopConsumer
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
         self.send({"type": "websocket.accept"})
-        print("WebSocket connected")
+        print("WebSocket connected", event)
 
     def websocket_receive(self, event):
-        print("Message received:", event.get('text'))
+        print("Message received:", event)
+        self.send({
+                "type": "websocket.send",
+                "text": "Hi, My friend!",
+            })
 
     def websocket_disconnect(self, event):
-        print("WebSocket disconnected")
+        print("WebSocket disconnected", event)
+        raise StopConsumer()
 
 
 class MyAsyncConsumer(AsyncConsumer):
@@ -21,6 +26,13 @@ class MyAsyncConsumer(AsyncConsumer):
 
     async def websocket_receive(self, event):
         print("Message received:", event.get('text'))
+        await self.send(
+            {
+                "type": "websocket.send",
+                "text": "Hi, My friend!"
+            }
+        )
 
     async def websocket_disconnect(self, event):
         print("WebSocket disconnected")
+        raise StopConsumer()
