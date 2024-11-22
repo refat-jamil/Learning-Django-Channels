bbd03fa07c82cf3dd9b9a74e6871a156851af4b7
Refat Jamil <rifatjamil54@gmail.com>
2024-11-22 23:05:42 +0600

    group sava in database

--- my_project/app/views.py
+++ my_project/app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from . models import Chat, Group
 
 def index(request, group_name):
     # Pass the group_name to the template context
+
+    group = Group.objects.filter(name = group_name).first()
+    chat = []
+    
+    if group:
+        chat = Chat.objects.filter(group=group)
+    else:    
+        group = Group(name=group_name)
+        group.save()
+
     context = {
-        'group_name': group_name
+        'group_name': group_name,
+        'chat':chat
     }
     return render(request, 'app/index.html', context)
